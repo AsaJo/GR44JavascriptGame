5d97586b8eab1512b2784c2d3e79297d49f3fc63
AsaJo <101865458+AsaJo@users.noreply.github.com>
2022-10-27 10:41:31 +0200

    Add files via upload

---
+++ GR44JavascriptGame/app.js
@@ -0,0 +1,45 @@
+"use strict"
+
+document.getElementById("welcomeH1").innerHTML= " Hi happy hackers!"
+
+var myDiv = document.getElementById("myDiv");
+
+myDiv.innerHTML="";
+var myBody = document.getElementsByTagName("body")[0];
+myBody.style.backgroundColor = "lightgray";
+console.log(myDiv);
+
+myDiv.style.width = "300px";
+myDiv.style.height = "300px";
+myDiv.style.backgroundColor="lightgreen";
+
+var chessToggle = true;
+var player ="X";
+
+for( let y = 1; y <=3; y++){
+    for(let x = 1; x <=3; x++){
+        const element = document.createElement("div");
+        element.className="divBox";
+
+        if (chessToggle){
+            element.classList.add("blackBox")
+        }
+        else{
+            element.classList.add("whiteBox")
+        }
+        chessToggle = !chessToggle;
+        element.id= "x" + x + "y" +y;
+        element.addEventListener("click", playerMove);
+        myDiv.appendChild(element);
+    }
+}
+function playerMove(event){
+    console.log(event);
+    event.target.innerHTML=player;
+
+    if(player === "X"){
+        player = "O";
+    }else{
+        player = "X";
+    }
+}
